814528e30b560a618d221342415159cf67f66c56
fitbouncer <bhuinjohn@gmail.com>
2023-02-05 18:13:58 +0530

    contacts displyed from database json server

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -5,38 +5,38 @@ export class ContactService{
     static serverURL  = 'http://localhost:9000';
 
     static getAllContacts(){
-        let dataURL = '$(this.serverURL)/contacts';
+        let dataURL = `${this.serverURL}/contacts`;
         return axios.get(dataURL);
     }
 
     static getContact(contactId){
-        let dataURL = '$(this.serverURL)/contacts/$(contactId)';
+        let dataURL = `${this.serverURL}/contacts/`+contactId;
         return axios.get(dataURL);
     }
 
     static createContact(contact){
-        let dataURL = '$(this.serverURL)/contacts/';
+        let dataURL = `${this.serverURL}/contacts`;
         return axios.post(dataURL,contact);
     }
 
     static updateContact(contact, contactId){
-        let dataURL = '$(this.serverURL)/contacts/$(contactId)';
+        let dataURL = `${this.serverURL}/contacts/`+contactId;
         return axios.put(dataURL,contact);
     }
 
     static deleteContact(contactId){
-        let dataURL = '$(this.serverURL)/contacts/$(contactId)';
+        let dataURL = `${this.serverURL}/contacts/`+contactId;
         return axios.delete(dataURL);
     }
 
-    static getAllGroups(contactId){
-        let dataURL = '$(this.serverURL)/groups';
+    static getAllGroups(){
+        let dataURL = `${this.serverURL}/groups`;
         return axios.delete(dataURL);
     }
 
     static getGroup(contact){
         let groupId = contact.groupId;
-        let dataURL = '$(this.serverURL)/groups/$(groupId)';
+        let dataURL = `${this.serverURL}/groups/`+groupId;
         return axios.delete(dataURL);
     }
 
--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -24,21 +24,41 @@
 
         </div>
     </div>
-
+    <!-- loding -->
+<div v-if="loading">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <LodingSpinner/>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- error -->
+<div v-if="!loading && errorMessage">
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col">
+                <p class="h4 text-danger fw-bold">{{ errorMessage }}</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+    <div class="container mt-5" v-if="contacts.length > 0">
+        <div class="row">
+            <div class="col-md-6" v-for="contact of contacts" :key="contact" >
                 <div class="card my-2 list-group-item-success shadow-lg">
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-md-4">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSJJSFcWEKr6nqzqWPn1KNSIqWTWJsiICKQLJFYQXIOjunf-nDlblfXPC3NNWGAz8HpsA&usqp=CAU" alt="image not found" class="contact-img">
+                                <img :src=" contact.photo" alt="image not found" class="contact-img">
                             </div>
                             <div class="col-md-7">
                                 <ul class="list-group">
-                                    <li class="list-group-item">Name : <span class="fw-bold">Pradipta Bhuin</span></li>
-                                    <li class="list-group-item">Email : <span class="fw-bold">pradipta@gmail.com</span></li>
-                                    <li class="list-group-item">Mobile : <span class="fw-bold">+91 9382950050</span></li>
+                                    <li class="list-group-item">Name : <span class="fw-bold">{{ contact.name }}</span></li>
+                                    <li class="list-group-item">Email : <span class="fw-bold">{{ contact.email }}</span></li>
+                                    <li class="list-group-item">Mobile : <span class="fw-bold">{{ contact.mobile }}</span></li>
 
                                 </ul>
                             </div>
@@ -59,7 +79,7 @@
             </div>
 
             <!-- 2nd -->
-            <div class="col-md-6">
+            <!-- <div class="col-md-6">
                 <div class="card my-2 list-group-item-success shadow-lg">
                     <div class="card-body">
                         <div class="row align-items-center">
@@ -88,9 +108,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <!-- 3rd -->
-            <div class="col-md-6">
+            <!-- <div class="col-md-6">
                 <div class="card my-2 list-group-item-success shadow-lg">
                     <div class="card-body">
                         <div class="row align-items-center">
@@ -119,9 +139,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <!-- 4th -->
-            <div class="col-md-6">
+            <!-- <div class="col-md-6">
                 <div class="card my-2 list-group-item-success shadow-lg shadow-md">
                     <div class="card-body">
                         <div class="row align-items-center">
@@ -150,12 +170,14 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
 
 <script>
+import LodingSpinner from '@/components/LodingSpinner.vue';
+import { ContactService } from '@/services/ContactService';
 export default {
     name: "ContactManager",
     data: function () {
@@ -163,8 +185,21 @@ export default {
             loading: false,
             contacts: [],
             errorMessage: null,
+        };
+    },
+    created: async function () {
+        try {
+            this.loading = true;
+            let response = await ContactService.getAllContacts();
+            this.contacts = response.data;
+            this.loading = false;
         }
-    }
+        catch (error) {
+            this.errorMessage = error;
+            this.loading = false;
+        }
+    },
+    components: { LodingSpinner }
 }
 </script>
 
